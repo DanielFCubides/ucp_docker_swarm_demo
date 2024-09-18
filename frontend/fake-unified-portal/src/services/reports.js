5ca8379d031519d1d707515e2d3949cb8198a7d3
Gustavo Saavedra <gsaavedra@telesign.com>
2024-09-18 16:58:34 -0500

    fixes port conflicts in compose file

--- frontend/fake-unified-portal/src/services/reports.js
+++ frontend/fake-unified-portal/src/services/reports.js
@@ -1,7 +1,7 @@
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'http://swarm.dfcubidesc.com:8000';
 
 export const getVisitors = async () => {
-    const result = await fetch(`${API_URL}/report/visits`, {
+    const result = await fetch(`${API_URL}/report/visits/`, {
         cache: 'no-store',
     });
     return await result.json();
