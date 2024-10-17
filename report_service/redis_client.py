0ab872d7aedfeb4d4d2f69cc61ff730aac8a23e3
DanielFCubides <dfcubidesc@gmail.com>
2024-10-16 19:45:39 -0500

    change the redis_client.py to use the clusterIP service and add local registry reference from the deploy compose yaml

--- report_service/redis_client.py
+++ report_service/redis_client.py
@@ -3,7 +3,7 @@ import redis
 
 
 def get_redis_client():
-    redis_host = os.environ.get("REDIS_HOST", "localhost")
+    redis_host = os.environ.get("REDIS_HOST", "redis-cache")
     redis_port = int(os.environ.get("REDIS_PORT", 6379))
 
     return redis.Redis(host=redis_host, port=redis_port, db=0)
